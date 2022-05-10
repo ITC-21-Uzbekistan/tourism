a8c5ccc521ca03ea40ee6065e5d6f6ac96e6d5d1
SirojiddinBakhadirov <105272105+SirojiddinBakhadirov@users.noreply.github.com>
2022-05-10 04:23:37 -0700

    comment

--- country/urls.py
+++ country/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from .views import get_countries, create_country, update_country, delete_country
 
-urlpatterns = []
+urlpatterns = [
+    path('get/', get_countries),
+    path('get/<int:pk>/', get_countries),
+    path('create/', create_country),
+    path('update/<int:pk>/', update_country),
+    path('delete/<int:pk>/', delete_country),
+]
--- country/views.py
+++ country/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_countries(request, pk=0):
+    if pk == 0:
+        return HttpResponse("Get all countries")
+    else:
+        return HttpResponse("Get detail {}".format(pk))
+
+
+@api_view(['POST'])
+def create_country(request):
+    return HttpResponse("Create")
+
+
+@api_view(['PUT'])
+def update_country(request, pk):
+    return HttpResponse("Update {}".format(pk))
+
+
+@api_view(['DELETE'])
+def delete_country(request, pk):
+    return HttpResponse("Delete {}".format(pk))
