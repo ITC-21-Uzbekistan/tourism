94ce29e59ea1900ab22fe1137c62dcef9750ba89
Bekzod <toshpulatovbekki7@icloud.com>
2022-05-30 11:11:20 +0500

    clear string

--- own_packages/clear_string.py
+++ own_packages/clear_string.py
@@ -1,8 +1,7 @@
-def clear_string(request):
-    return "OK"
+def clear_string(text):
+    return_word = text
+    for char in text:
+        if str(char) in "!@#$%^&*()_+=?/-<>,.';:][{} ":
+            return_word = return_word.replace(char, "")
 
-
-# arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9]
-#
-# for i in range(len(arr)):
-#     print(arr[i])
+    return str(return_word).lower()
